ddfbf88474e2ba8a59d153c1a26b6f261522cbed
bmcminn <brandtley@giggleboxstudios.net>
2014-12-04 13:07:17 -0600

    added stringContains utility that wraps strpos()

---
+++ src/__/utilities/stringContains.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace utilities;
+
+/**
+ * @name   stringContains()
+ * Readable wrapper for strpos()
+ * @param  string  $needle   Substring to search for
+ * @param  string  $haystack String to search within
+ * @param  integer $offset   Index of the $haystack we wish to start at
+ * @return bool              whether the
+ */
+function stringContains($needle, $haystack, $offset=0) {
+  return strpos($haystack, $needle, $offset) !== false;
+}
--- tests/utilities.php
+++ tests/utilities.php
@@ -24,4 +24,25 @@ class UtilitiesTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(true, is_numeric($x));
     }
 
+
+    public function testStringContains() {
+        // Arrange
+        $a  = 'testi';
+        $b  = 'testing wafflecones';
+        $a2 = 'waffle';
+
+        // Act
+        $x  = __::stringContains($a, $b);
+        $x2 = __::stringContains($a, $b, 5);
+        $x3 = __::stringContains($a2, $b);
+        $x4 = __::stringContains($a2, $b, 7);
+
+        // Assert
+        $this->assertEquals(true, $x);
+        $this->assertEquals(false, $x2);
+        $this->assertEquals(true, $x3);
+        $this->assertEquals(true, $x4);
+
+    }
+
 }
