827b27f42f76676db1ed001dbc1df1c01f3a3003
Ivan Ternovtsiy <diaborn19@gmail.com>
2017-01-05 19:54:51 +0200

    micro improvement of repeat method

--- src/__/arrays/repeat.php
+++ src/__/arrays/repeat.php
@@ -8,13 +8,13 @@ namespace arrays;
  ** __::repeat('foo', 3);
  ** // → ['foo', 'foo', 'foo']
  *
- * @param string   $object The object to repeat.
- * @param int|null $times  How many times has to be repeated.
+ * @param string $object The object to repeat.
+ * @param int    $times  How many times has to be repeated.
  *
  * @return array Returns a new array of filled values.
  *
  */
 function repeat($object, $times)
 {
-    return $times ? \array_fill(0, (int)$times, $object) : [];
+    return \array_fill(0, (int)$times, $object);
 }
