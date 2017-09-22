7b0bed0ea8f2b76869e008a1949d982e9575fd93
Yoan Tournade <yoan@ytotech.com>
2017-09-22 21:32:21 +0200

    Remove debug code

--- src/__/strings/words.php
+++ src/__/strings/words.php
@@ -68,10 +68,11 @@ function words($input, $pattern = null)
     $asciiWords = '/[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/';
 
     $hasUnicodeWordRegex = '/[a-z][A-Z]|[A-Z]{2,}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/';
-    
+
     $rsOptJoin = '(?:' . $rsZWJ . '(?:' . join('|', [$rsNonAstral, $rsRegional, $rsSurrPair]) . ')' . $rsOptVar . $reOptMod . ')*';
     $rsSeq = $rsOptVar . $reOptMod . $rsOptJoin;
     $rsEmoji = '(?:' . join('|', [$rsDingbat, $rsRegional, $rsSurrPair]) . ')' . $rsSeq;
+
     /**
      * Splits a Unicode `string` into an array of its words.
      *
@@ -93,7 +94,6 @@ function words($input, $pattern = null)
         ]
     ) . '/u';
     if ($pattern === null) {
-        // var_dump($unicodeWords);
         $hasUnicodeWord = \preg_match($hasUnicodeWordRegex, $input);
         $pattern = $hasUnicodeWord ? $unicodeWords : $asciiWords;
     }
