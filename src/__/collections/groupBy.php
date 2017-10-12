9625770c15a7e5d5a36b14a0865636d792ec91bc
Yoan Tournade <yoan@ytotech.com>
2017-10-12 14:24:54 +0200

    Add some PHPDoc for groupBy

--- src/__/collections/groupBy.php
+++ src/__/collections/groupBy.php
@@ -8,6 +8,43 @@ namespace collections;
  * Based on {@author Chauncey McAskill}'s {@link https://gist.github.com/mcaskill/baaee44487653e1afc0d array_group_by()}
  * function.
  *
+ ** __::groupBy([
+ **         ['state' => 'IN', 'city' => 'Indianapolis', 'object' => 'School bus'],
+ **         ['state' => 'CA', 'city' => 'San Diego', 'object' => 'Light bulb'],
+ **         ['state' => 'CA', 'city' => 'Mountain View', 'object' => 'Space pen'],
+ **     ],
+ **     'state'
+ ** );
+ ** // >> [
+ ** //   'IN' => [
+ ** //      ['state' => 'IN', 'city' => 'Indianapolis', 'object' => 'School bus'],
+ ** //      ['state' => 'CA', 'city' => 'San Diego', 'object' => 'Light bulb'],
+ ** //   ],
+ ** //   'CA' => [
+ ** //      ['state' => 'CA', 'city' => 'Mountain View', 'object' => 'Space pen']
+ ** //   ]
+ ** // ]
+ *
+ *
+ ** __::groupBy([
+ **         ['state' => 'IN', 'city' => 'Indianapolis', 'object' => 'School bus'],
+ **         ['state' => 'IN', 'city' => 'Indianapolis', 'object' => 'Manhole'],
+ **         ['state' => 'CA', 'city' => 'San Diego', 'object' => 'Light bulb'],
+ **     ],
+ **     function ($value) {
+ **         return $value->city;
+ **     }
+ ** );
+ ** // >> [
+ ** //   'Indianapolis' => [
+ ** //      ['state' => 'IN', 'city' => 'Indianapolis', 'object' => 'School bus'],
+ ** //      ['state' => 'IN', 'city' => 'Indianapolis', 'object' => 'Manhole'],
+ ** //   ],
+ ** //   'San Diego' => [
+ ** //      ['state' => 'CA', 'city' => 'San Diego', 'object' => 'Light bulb'],
+ ** //   ]
+ ** // ]
+ *
  * @param array $array
  * @param int|float|string|\Closure $key
  *
