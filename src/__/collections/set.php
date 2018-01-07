2c564afa530635410428a98e5cfb4b3350f2335a
Yoan Tournade <yoan@ytotech.com>
2018-01-07 06:27:12 +0100

    Fix issue with passing argument by reference in __::set(): need & in both calling code and function parameter definition

--- src/__/collections/set.php
+++ src/__/collections/set.php
@@ -3,38 +3,52 @@
 namespace collections;
 
 /**
- * Set item of an array by index to given value, accepting nested index
+ * Set item of a collection by index to given value, accepting nested index.
  *
  ** __::set(['foo' => ['bar' => 'ter']], 'foo.baz.ber', 'fer');
  ** // → '['foo' => ['bar' => 'ter', 'baz' => ['ber' => 'fer']]]'
  *
- * @param array   $collection array of values
- * @param string  $key        key or index
+ * @param array|object $collection collection of values
+ * @param string  $path        key or index
  * @param mixed   $value      the value to set at position $key
  * @param boolean $strict     if the path should be generated even if the path consists of non collections
  * @throws \Exception if the path consists of a non collection and strict is set to false
  *
- * @return array the manipulated array
+ * @return array|object the manipulated collection
  *
  */
-function set(array $collection, $key, $value = null, $strict = false)
+function set($collection, $keys, $value = null, $strict = false)
 {
-    if ($key === null) {
+    $set_object = function (&$object, $key, $value) {
+        $object->$key = $value;
+        return $object;
+    };
+    $set_array = function (&$array, $key, $value) {
+        $array[$key] = $value;
+        return $array;
+    };
+    $setter = \__::isObject($collection) ? $set_object : $set_array;
+
+    if ($keys === null) {
         return $collection;
     }
 
-    $keys = \explode('.', $key);
+    $keys = \explode('.', $keys);
     $key  = \array_shift($keys);
 
     if (\count($keys) === 0) {
-        $collection[$key] = $value;
+        call_user_func_array($setter, [&$collection, $key, $value]);
+        // $collection = call_user_func_array($setter, [&$collection, $key, $value]);
     } else {
-        if (!\array_key_exists($key, $collection) || (!\is_array($collection[$key]) && $strict)) {
+        if (!\__::has($collection, $key) || (!\is_array(\__::get($collection, $key)) && $strict)) {
             $collection[$key] = [];
-        } elseif (!\is_array($collection[$key])) {
+        } elseif (!\is_array(\__::get($collection, $key))) {
             throw new \Exception(sprintf('Could not insert value %s into array because the value at key %s is no array.', $value, $key));
         }
-        $collection[$key] = set($collection[$key], implode('.', $keys), $value);
+        call_user_func_array(
+            $setter,
+            [&$collection, $key, set(\__::get($collection, $key), implode('.', $keys), $value)]
+        );
     }
 
     return $collection;
