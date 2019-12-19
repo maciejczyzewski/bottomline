30dd909be345215812796981aaf3e9293c49ce6e
Vladimir Jimenez <allejo@me.com>
2019-12-18 22:25:40 -0800

    Let `drop` advance an iterator or slice an array

--- src/__/arrays/drop.php
+++ src/__/arrays/drop.php
@@ -17,12 +17,32 @@ namespace arrays;
  * [3, 5]
  * ```
  *
- * @param array $input The array to query.
- * @param int $number The number of elements to drop.
+ * @param array|iterable $input  The array or iterable to query.
+ * @param int            $number The number of elements to drop.
  *
- * @return array
+ * @throws \Exception
+ *
+ * @return array|iterable
  */
-function drop(array $input, $number = 1)
+function drop(/*iterable*/ $input, $number = 1)
 {
-    return array_slice($input, $number);
+    if (is_array($input)) {
+        return array_slice($input, $number);
+    }
+
+    if ($input instanceof \Iterator) {
+        for ($i = 0; $i < $number; $i++) {
+            $input->next();
+        }
+
+        return $input;
+    }
+
+    if ($input instanceof \IteratorAggregate) {
+        $itr = $input->getIterator();
+
+        return drop($itr, $number);
+    }
+
+    throw new \InvalidArgumentException('$input should implement the Iterator or IteratorAggregate interface');
 }
