92ab27ebeda4129ddda0a7630e0216e3c2c90494
Yoan Tournade <y@mandelbro.com>
2020-01-01 15:07:11 +0100

    Use iterator_to_array for reading into memory the traversable to reverse

--- src/__/collections/reverseIterable.php
+++ src/__/collections/reverseIterable.php
@@ -3,7 +3,10 @@
 namespace collections;
 
 /**
- * Return the reverse of an array or other foreach-able (Iterable) without making a copy of it.
+ * Return the reverse of an array or other foreach-able (Iterable).
+ *
+ * For array it does not make a copy of it; but does make a copy to memory for other
+ * traversables.
  *
  * Code (using `yield`) is from mpen and linepogl
  * See https://stackoverflow.com/a/36605605/1956471
@@ -25,22 +28,7 @@ namespace collections;
  */
 function reverseIterable($iterable)
 {
-    // $iterable_values = [];
-    // if (is_array($iterable)) {
-    //     // TODO Use array_reverse?
-    //     $iterable_values = $iterable;
-    // } else {
-    //     // We first read the iterable values to an array.
-    //     // We do that as there is no API to read an iterable from end to start
-    //     // (because it could be an infinite genrator)
-    //     // and that generators can be iterated only once.
-    //     // So it seems we indeed need to pull all iterable content into memory
-    //     // for reversing it.
-    //     foreach ($iterable as $key => $value) {
-    //         $iterable_values[$key] = $value;
-    //     }
-    // }
-    $iterable_values = $iterable;
+    $iterable_values = is_array($iterable) ? $iterable : \iterator_to_array($iterable, true);
     for (end($iterable_values); ($key = key($iterable_values)) !== null; prev($iterable_values)) {
         yield $key => current($iterable_values);
     }
