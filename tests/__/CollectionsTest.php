d5f210e018718b7c642b1e1af20dee2c49ea90d0
Vladimir Jimenez <allejo@me.com>
2020-01-04 14:02:24 -0800

    Add iterable support for `concat()`

--- tests/__/CollectionsTest.php
+++ tests/__/CollectionsTest.php
@@ -46,44 +46,139 @@ class CollectionsTest extends TestCase
         $this->assertEquals((object)['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 6], $y);
     }
 
-    public function testConcat()
+    public static function dataProvider_concat()
     {
-        // Arrange
-        $a1 = ['color' => ['favorite' => 'red', 5], 3];
-        $a2 = [10, 'color' => ['favorite' => 'green', 'blue']];
-        $b1 = ['a' => 0];
-        $b2 = ['a' => 1, 'b' => 2, 5];
-        $b3 = ['c' => 3, 'd' => 4, 6];
-        $c1 = [1, 2, 3];
-        $c2 = [4, 5];
-
-        // Act
-        $x = __::concat($a1, $a2);
-        $y = __::concat($b1, $b2, $b3);
-        $z = __::concat($c1, $c2);
-
-        // Assert
-        $this->assertEquals(['color' => ['favorite' => 'green', 'blue'], 3, 10], $x);
-        $this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 5, 6], $y);
-        $this->assertEquals([1, 2, 3, 4, 5], $z);
+        return [
+            // Test with multiple arrays should return an array
+            [
+                'sources' => [
+                    ['color' => ['favorite' => 'red', 5], 3],
+                    [10, 'color' => ['favorite' => 'green', 'blue']],
+                ],
+                'expected' => ['color' => ['favorite' => 'green', 'blue'], 3, 10],
+            ],
+            // Test with multiple objects should return an object
+            [
+                'sources' => [
+                    (object)['color' => (object)['favorite' => 'red', 5]],
+                    (object)[10, 'color' => (object)['favorite' => 'green', 'blue']],
+                ],
+                'expected' => (object)['color' => (object)['favorite' => 'green', 'blue'], 10],
+            ],
+            // Test three objects returns an object
+            [
+                'sources' => [
+                    (object)['a' => 0],
+                    (object)['a' => 1, 'b' => 2],
+                    (object)['c' => 3, 'd' => 4],
+                ],
+                'expected' => (object)['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4],
+            ],
+            // Test arrays and objects mixing together, returns array because 1st element is array
+            [
+                'sources' => [
+                    ['a' => 0],
+                    (object)['a' => 1, 'b' => 2],
+                    (object)['c' => 3, 'd' => 4],
+                ],
+                'expected' => ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4],
+            ],
+            // Test arrays and objects mixing together, returns object because 1st element is object
+            [
+                'sources' => [
+                    (object)['a' => 0],
+                    ['a' => 1, 'b' => 2],
+                    ['c' => 3, 'd' => 4],
+                ],
+                'expected' => (object)['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4],
+            ],
+            [
+                'sources' => [
+                    ['a' => 0],
+                    ['a' => 1, 'b' => 2, 5],
+                    ['c' => 3, 'd' => 4, 6],
+                ],
+                'expected' => ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 5, 6],
+            ],
+            // Test concat with arrays and iterables
+            [
+                'sources' => [
+                    ['a' => 0],
+                    new ArrayIterator(['a' => 1, 'b' => 2, 5]),
+                    new MockIteratorAggregate(['c' => 3, 'd' => 4, 6]),
+                ],
+                'expected' => ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 5, 6],
+            ],
+            // Test on arrays with no keys just append
+            [
+                'sources' => [
+                    [1, 2, 3],
+                    [4, 5],
+                ],
+                'expected' => [1, 2, 3, 4, 5],
+            ],
+            // Test iterator with an array
+            [
+                'sources' => [
+                    new ArrayIterator([1, 2, 3]),
+                    [4, 5],
+                ],
+                'expected' => [1, 2, 3, 4, 5],
+            ],
+            [
+                'sources' => [
+                    new MockIteratorAggregate([1, 2, 3]),
+                    (object)[4, 5],
+                ],
+                'expected' => [1, 2, 3, 4, 5],
+            ],
+            // Test generator and array
+            [
+                'sources' => [
+                    call_user_func(function () {
+                        yield 1;
+                        yield 2;
+                        yield 3;
+                    }),
+                    [4, 5, 6],
+                ],
+                'expected' => [1, 2, 3, 4, 5, 6],
+            ],
+            // Test all generators
+            [
+                'sources' => [
+                    call_user_func(function () {
+                        yield 1;
+                        yield 2;
+                        yield 3;
+                    }),
+                    call_user_func(function () {
+                        yield 4;
+                        yield 5;
+                        yield 6;
+                    }),
+                    call_user_func(function () {
+                        yield 7;
+                        yield 8;
+                        yield 9;
+                    }),
+                ],
+                'expected' => [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            ],
+        ];
     }
 
-    public function testConcatObject()
+    /**
+     * @dataProvider dataProvider_concat
+     *
+     * @param array $sources
+     * @param array $expected
+     */
+    public function testConcat($sources, $expected)
     {
-        // Arrange
-        $a1 = (object)['color' => (object)['favorite' => 'red', 5]];
-        $a2 = (object)[10, 'color' => (object)['favorite' => 'green', 'blue']];
-        $b1 = (object)['a' => 0];
-        $b2 = (object)['a' => 1, 'b' => 2];
-        $b3 = (object)['c' => 3, 'd' => 4];
+        $actual = call_user_func_array('__::concat', $sources);
 
-        // Act
-        $x = __::concat($a1, $a2);
-        $y = __::concat($b1, $b2, $b3);
-
-        // Assert
-        $this->assertEquals((object)['color' => (object)['favorite' => 'green', 'blue'], 10], $x);
-        $this->assertEquals((object)['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], $y);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testConcatDeep()
