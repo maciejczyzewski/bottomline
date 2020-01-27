ed41c8424523a3bfbcfd0ce35e24a7929d12832b
Vladimir Jimenez <allejo@me.com>
2020-01-27 00:04:04 -0800

    Change __::mapValues() to maybe return a Generator

--- src/__/collections/mapValues.php
+++ src/__/collections/mapValues.php
@@ -3,6 +3,21 @@
 namespace collections;
 
 /**
+ * @internal
+ *
+ * @param iterable $iterable
+ * @param \Closure $closure
+ *
+ * @return \Generator
+ */
+function mapValuesIterable($iterable, $closure)
+{
+    foreach ($iterable as $key => $value) {
+        yield $key => call_user_func_array($closure, [$value, $key, $iterable]);
+    }
+}
+
+/**
  * Transforms the values in a collection by running each value through the iterator.
  *
  * **Usage**
@@ -20,18 +35,19 @@ namespace collections;
  * ```
  *
  * @param array|iterable $array       Array of values
- * @param \Closure       $closure     Closure to map the values
+ * @param \Closure|null  $closure     Closure to map the values
  *
- * @return array
+ * @return array|\Generator
  */
 function mapValues($array, \Closure $closure = null)
 {
     if (is_null($closure)) {
         $closure = '__::identity';
     }
-    $resultArray = [];
-    foreach ($array as $key => $value) {
-        $resultArray[$key] = call_user_func_array($closure, array($value, $key, $array));
+
+    if (is_array($array)) {
+        return iterator_to_array(mapValuesIterable($array, $closure));
     }
-    return $resultArray;
+
+    return mapValuesIterable($array, $closure);
 }
--- tests/__/CollectionsTest.php
+++ tests/__/CollectionsTest.php
@@ -1415,90 +1415,133 @@ class CollectionsTest extends TestCase
         });
     }
 
-    public function testMapValues()
+    public function dataProvider_mapValues()
     {
-        // Arrange
-        $a = [
-            'name1' => [
-                'name' => 'Tuan',
-                'age' => 26
+        return [
+            [
+                'source' => [
+                    'name1' => [
+                        'name' => 'Tuan',
+                        'age' => 26
+                    ],
+                    'name2' => [
+                        'name' => 'Nguyen',
+                        'age' => '25'
+                    ],
+                ],
+                'closure' => function ($value) {
+                    return array_flip($value);
+                },
+                'isGenerator' => false,
+                'expected' => [
+                    'name1' => [
+                        'Tuan' => 'name',
+                        26 => 'age'
+                    ],
+                    'name2' => [
+                        'Nguyen' => 'name',
+                        25 => 'age'
+                    ],
+                ],
             ],
-            'name2' => [
-                'name' => 'Nguyen',
-                'age' => '25'
+            [
+                'source' => [
+                    'name1' => [
+                        'name' => 'Tuan',
+                        'age' => 26
+                    ],
+                    'name2' => [
+                        'name' => 'Nguyen',
+                        'age' => '25'
+                    ],
+                ],
+                'closure' => function ($value, $key, $collection) {
+                    $size = count($collection);
+
+                    return [
+                        'subKey' => "{$value['age']}_{$key}_{$size}"
+                    ];
+                },
+                'isGenerator' => false,
+                'expected' => [
+                    'name1' => ['subKey' => '26_name1_2'],
+                    'name2' => ['subKey' => '25_name2_2'],
+                ],
+            ],
+            [
+                'source' => [
+                    'name1' => [
+                        'name' => 'Tuan',
+                        'age' => 26
+                    ],
+                    'name2' => [
+                        'name' => 'Nguyen',
+                        'age' => '25'
+                    ],
+                ],
+                'closure' => null,
+                'isGenerator' => false,
+                'expected' => [
+                    'name1' => [
+                        'name' => 'Tuan',
+                        'age' => 26
+                    ],
+                    'name2' => [
+                        'name' => 'Nguyen',
+                        'age' => '25'
+                    ],
+                ],
+            ],
+            [
+                'source' => new ArrayIterator([
+                    'name1' => [
+                        'name' => 'Tuan',
+                        'age' => 26
+                    ],
+                    'name2' => [
+                        'name' => 'Nguyen',
+                        'age' => '25'
+                    ],
+                ]),
+                'closure' => function ($value) {
+                    return array_flip($value);
+                },
+                'isGenerator' => true,
+                'expected' => [
+                    'name1' => [
+                        'Tuan' => 'name',
+                        26 => 'age'
+                    ],
+                    'name2' => [
+                        'Nguyen' => 'name',
+                        25 => 'age'
+                    ],
+                ],
             ],
         ];
-
-        // Act
-        $b = __::mapValues($a, function ($value) {
-            return array_flip($value);
-        });
-
-        // Assert
-        $this->assertEquals([
-            'name1' => [
-                'Tuan' => 'name',
-                26 => 'age'
-            ],
-            'name2' => [
-                'Nguyen' => 'name',
-                25 => 'age'
-            ],
-        ], $b);
-
-        // test with complicated closure
-        // Act
-        $b = __::mapValues($a, function ($value, $key, $collection) {
-            $size = count($collection);
-            return [
-                'subKey' => "{$value['age']}_{$key}_{$size}"
-            ];
-        });
-
-        // Assert
-        $this->assertEquals([
-            'name1' => ['subKey' => '26_name1_2'],
-            'name2' => ['subKey' => '25_name2_2'],
-        ], $b);
-
-        // test when closure is null, the returned array should have the same data with the original array
-        // Act
-        $b = __::mapValues($a);
-
-        // Assert
-        $this->assertEquals($a, $b);
     }
 
-    public function testMapValuesIterable()
+    /**
+     * @dataProvider dataProvider_mapValues
+     *
+     * @param iterable      $source
+     * @param \Closure|null $closure
+     * @param bool          $isGenerator
+     * @param array         $expected
+     */
+    public function testMapValues($source, $closure, $isGenerator, $expected)
     {
-        // Arrange
-        $a = new ArrayIterator([
-            'name1' => [
-                'name' => 'Tuan',
-                'age' => 26
-            ],
-            'name2' => [
-                'name' => 'Nguyen',
-                'age' => '25'
-            ],
-        ]);
+        $actual = __::mapValues($source, $closure);
 
-        // Act
-        $b = __::mapValues($a, function ($value) {
-            return array_flip($value);
-        });
+        if ($isGenerator) {
+            $this->assertInstanceOf(\Generator::class, $actual);
+        } else {
+            $this->assertTrue(is_array($actual));
+        }
 
-        // Assert
-        $this->assertEquals([
-            'name1' => [
-                'Tuan' => 'name',
-                26 => 'age'
-            ],
-            'name2' => [
-                'Nguyen' => 'name',
-                25 => 'age'
-            ],
-        ], $b);
+        foreach ($actual as $key => $item) {
+            $this->assertEquals($expected[$key], $item);
+        }
     }
 
     public function testMax()
